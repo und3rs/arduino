beae05b1c89ea5b12987e50e99d61f219bd6ed6a
sungwook <sw0826.kim@naverz-corp.com>
2024-10-14 22:42:07 +0900

    feat: fix

--- NightLight/NightLight.ino
+++ NightLight/NightLight.ino
@@ -8,7 +8,7 @@
 #define PIN_START PB2
 #define PIN_LED PB0
 
-#define TEIMER_SEC 10
+#define TEIMER_SEC 600
 
 SimpleTimer timer;
 int timerId = 0;
@@ -54,7 +54,7 @@ void tick() {
     if(seconds >= TEIMER_SEC) {
       sleepMode();
     } else {
-      analogWrite(PIN_LED, calcBrightness());
+      //analogWrite(PIN_LED, calcBrightness());
     }
   }
 }
@@ -83,7 +83,9 @@ void ledMinus() {
 
 int calcBrightness() {
   int brightness = BRIGHTNESS[brightnessIdx];
-  return max(int(brightness * (1.0f - (seconds / float(TEIMER_SEC)))), BRIGHTNESS[0]);
+  return brightness;
+  // brightness = int(brightness * (1.0f - (seconds / float(TEIMER_SEC)));- 
+  // return max(brightness, BRIGHTNESS[0]);
 }
 
 
@@ -98,11 +100,21 @@ void sleepMode()
   timer.disable(timerId);
 
   
-  // set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  // sleep_enable();
-  // sleep_mode();
+  /*
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  cli();
+  GIMSK = 0b00100000;
+  PCMSK = 0b00000100;
+  sei();
+  sleep_mode();
 
-  // sleep_disable();
+  sleep_disable();
+  cli();
+  GIMSK = 0b00100000;
+  PCMSK = 0b00011100;
+  sei();
+  */
 }
 
 
@@ -121,7 +133,7 @@ ISR(PCINT0_vect)
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
 
-  if (interrupt_time - last_interrupt_time > 500) {
+  if (interrupt_time - last_interrupt_time > 350) {
     if(digitalRead(PIN_START) == LOW) {
       if(!isRunning) {
         wakeUp();
