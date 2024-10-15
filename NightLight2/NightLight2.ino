4f91a6d64f6a594c10ab95fd17c4d91b27f7de41
sungwook <sw0826.kim@naverz-corp.com>
2024-10-15 14:19:14 +0900

    fix: interrupt bugs

--- NightLight2/NightLight2.ino
+++ NightLight2/NightLight2.ino
@@ -1,9 +1,11 @@
 #include <avr/sleep.h>
 #include "SimpleTimer.h"
 
+
 #define PIN_LED 3
 #define PIN_UP A0
 #define PIN_DOWN A1
+#define PIN_POWER 2
 
 #define TIME_OUT 5
 
@@ -15,7 +17,6 @@ volatile float brightness = 255;
 SimpleTimer timer;
 volatile int timerId = 0;
 int passedTimeInSec = 0;
-volatile bool isRunning = false;
 
 
 void setup() {
@@ -28,8 +29,9 @@ void setup() {
   pinMode(PIN_UP, INPUT_PULLUP);
   pinMode(PIN_DOWN, INPUT_PULLUP);
 
-  pinMode(2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), toggleOnOff, FALLING);
+  pinMode(PIN_POWER, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(PIN_POWER), toggleOnOff, FALLING);
 
   stop();
 }
@@ -42,7 +44,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   updateBrightness();
   analogWrite(PIN_LED, brightness);
-  if(isRunning == false) { return; }
 
   timer.run();
 
@@ -68,54 +69,64 @@ void loop() {
 
 
 void start() {
-  if(isRunning == false) {
-    #ifdef DEBUG
-    Serial.println("Start()");
-    #endif
-    isRunning = true;
-    passedTimeInSec = 0;
-    timerId = timer.setInterval(1000, tick);
-    timer.enable(timerId);
-    updateBrightness();
-  }
+  #ifdef DEBUG
+  Serial.println("\n\n>> START <<");
+  #endif
+  
+  passedTimeInSec = 0;
+  timerId = timer.setInterval(1000, tick);
+  timer.enable(timerId);
+  updateBrightness();
 }
 
 
 
 void stop() {
-  if(isRunning == true) {
-    #ifdef DEBUG
-    Serial.println("Stop()");
-    #endif
-    isRunning = false;
-    timer.disable(timerId);
-    timer.deleteTimer(timerId);
-    passedTimeInSec = TIME_OUT;
-    updateBrightness();
-    analogWrite(PIN_LED, brightness);
-  }
+  timer.disable(timerId);
+  timer.deleteTimer(timerId);
+  passedTimeInSec = TIME_OUT;
+  updateBrightness();
+  analogWrite(PIN_LED, brightness);
+
+  #ifdef DEBUG
+  Serial.println(">> SLEEP");
+  #endif
+  sleep_enable();  
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_cpu();
+
+  // woke up
+  start();
+  #ifdef DEBUG
+  Serial.println(">> WOKEUP");
+  #endif
+  sleep_disable(); 
 }
 
 
 
 int updateBrightness() {
-  brightness = isRunning ? ((255.0 * ((TIME_OUT - passedTimeInSec) / float(TIME_OUT))) * brightnessScale) : 0;
+  brightness = ((255.0 * ((TIME_OUT - passedTimeInSec) / float(TIME_OUT))) * brightnessScale);
 }
 
 
 
 void tick() {
+  #ifdef DEBUG
   Serial.print("[+Tick]");
-  if(isRunning) {
-    passedTimeInSec++;
-    #ifdef DEBUG
-    Serial.print("[Time]: ");
-    Serial.println(passedTimeInSec);
-    #endif
+  #endif
 
-    if(passedTimeInSec >= TIME_OUT && isRunning) {
-      stop();
-    }
+  passedTimeInSec++;
+  #ifdef DEBUG
+  Serial.print("[Time]: ");
+  Serial.println(passedTimeInSec);
+  #endif
+
+  if(passedTimeInSec >= TIME_OUT) {
+    #ifdef DEBUG
+    Serial.println(">> STOP");
+    #endif
+    stop();
   }
 }
 
@@ -126,7 +137,11 @@ void toggleOnOff()
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
   if (interrupt_time - last_interrupt_time > 500) {
-    !isRunning ? start() : stop();
-    last_interrupt_time = interrupt_time;
+    if(passedTimeInSec > 0) {
+      #ifdef DEBUG
+      Serial.println("[BUTTON] STOP");
+      #endif
+      passedTimeInSec = TIME_OUT;
+    }
   }
 }
